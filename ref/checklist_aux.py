0d3689ecc033653eed51edb3c4a1968b8e3d0d5c
Schneiderella <barbara.psch@gmail.com>
2023-10-18 22:14:33 -0300

    feat: checklist

---
+++ ref/checklist_aux.py
@@ -0,0 +1,22 @@
+# # Checklist:
+# - [x] Inclui emissor na lista de emissores
+# - [x] Remove emissor da lista de emissores
+# - [x] Consulta emissores: escreve na tela os emissores cadastrados
+# - [x] Inclui receptor na lista de receptores
+# - [x] Remove receptor da lista de receptores: quando o receptor é removido, sua fila de mensagens também é removida
+# - [x] Consulta receptores: escreve na tela os receptores cadastrados
+# - [x] Envia mensagem: um receptor cadastrado escreve a mensagem, e define quais são os receptores (pelo menos um receptor). A mensagem é enfileirada na fila de cada receptor.
+# - [x] Retira mensagem: um receptor cadastrado desenfileira uma mensagem (apresenta mensagem de erro se a fila está vazia)
+# - [x] Consulta fila de mensagens: exibe a fila de mensagens de um receptor
+# - [x] Outras operações (a critério), como: validar receptor, validar emissor, consultar qtd de mensagens por receptor.
+
+# ## Representação:
+# - [x] Usa listas encadeadas (simples).
+# - [x] Cada nodo da lista de emissores deve conter: número de identificação (inteiro), nome (string).
+# - [x] Cada nodo da lista de receptores deve conter: número de identificação (inteiro), nome (string)
+# - [x] Cada fila de mensagens deve conter: identificação do receptor (inteiro), e a fila de mensagens.
+# - [x] Cada nodo da fila de mensagens deve conter: a identificação do emissor (inteiro), e a mensagem (texto).
+
+# ## Fazer upload do trabalho na tarefa correspondente no AVA do trabalho com:
+# - [x] Código-fonte da implementação,.
+# - [x] Explicação textual (ou figura) sobre a estrutura de dados utilizada (como é cada lista: todas as listas são simples e há um header com o início de cada lista;
